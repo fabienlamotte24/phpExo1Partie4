3e63ef72aba4a7d6039ab1b5a8ab3fe73ad506ed
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-27 09:16:10 +0200

    exo1p4 php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 1</title>
+  </head>
+  <body>
+    <p>
+<?php
+  function returnTrue(){
+    return true;
+  }
+  echo returnTrue();
+ ?>
+    </p>
+  </body>
+</html>
